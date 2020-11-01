71a4265923ec6f8ed5ec98e0824f09706299448f
Jose Guaura <jguaura@8amarketing.com>
2020-11-01 19:33:51 -0300

    b

---
+++ build/precache-manifest.1b1d310f4707964da2c9a640fbafc175.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "073184d5d9ff6652d84acb988cab95ee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0726e2e560b2d14a48a9",
+    "url": "/static/css/2.2654a9ff.chunk.css"
+  },
+  {
+    "revision": "6ff29eb86a21c3176e29",
+    "url": "/static/css/main.dfb1764e.chunk.css"
+  },
+  {
+    "revision": "0726e2e560b2d14a48a9",
+    "url": "/static/js/2.e7c46c0d.chunk.js"
+  },
+  {
+    "revision": "68eb484dce045119a52132a8eb902e52",
+    "url": "/static/js/2.e7c46c0d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ff29eb86a21c3176e29",
+    "url": "/static/js/main.808c3434.chunk.js"
+  },
+  {
+    "revision": "3ff5d6fb19a2b7a4acf2",
+    "url": "/static/js/runtime-main.8b39ddf8.js"
+  }
+]);
